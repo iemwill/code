e11a17b65299685d5d57991cab69ac56dee5a0f6
iemwill <hello@laubenheimer.eu>
2021-06-15 20:41:24 +0200

    Add verification process.

--- pow-ex-opt.py
+++ pow-ex-opt.py
@@ -3,6 +3,8 @@ from time import perf_counter
 
 txs = b"Assume this are transactions we want to mine for our block, which we already have checked to be all valid. Now we need some leading zeroes of the SHA256."
 leading_zeroes = ""
+verify = []
+computation_time = 0
 
 for i in range(1,8):
 	print(str(i) + " leading zeroes:")
@@ -16,7 +18,19 @@ for i in range(1,8):
 		else:
 			nonce += 1
 	end_time = perf_counter()
+	verify.append([sha256(txs + str(nonce).encode()).hexdigest(), nonce])
 	print(sha256(txs + str(nonce).encode()).hexdigest())
 	print("nonce: " + str(nonce))
 	print ("Execution-time in seconds: " + str(end_time - start_time))
+	computation_time += (end_time - start_time)
 	print("--------------------------------------------------------------")
+
+print("Verification:")
+start_time = perf_counter()
+for i in verify:
+	print("Safed hash and nonce: ", i)
+	print("New computed hash with safed nonce: " + sha256(txs + str(i[1]).encode()).hexdigest())
+end_time = perf_counter()
+print("--------------------------------------------------------------")
+print("Computation process in seconds: " + str(computation_time))
+print("Verification process in seconds: " + str(end_time - start_time))
--- pow-ex.py
+++ pow-ex.py
@@ -2,6 +2,8 @@ from hashlib import sha256
 from time import perf_counter
 
 txs = b"Assume this are transactions we want to mine for our block, which we already have checked to be all valid. Now we need some leading zeroes of the SHA256."
+verify = []
+computation_time = 0
 
 print("One leading zero:")
 start_time = perf_counter()
@@ -13,9 +15,11 @@ while(newTry):
 	else:
 		nonce += 1
 end_time = perf_counter()
+verify.append([sha256(txs + str(nonce).encode()).hexdigest(), nonce])
 print(sha256(txs + str(nonce).encode()).hexdigest())
 print("nonce: " + str(nonce))
 print ("Execution-time in seconds: " + str(end_time - start_time))
+computation_time += (end_time - start_time)
 print("--------------------------------------------------------------")
 
 print("Two leading zeroes:")
@@ -28,9 +32,11 @@ while(newTry):
 	else:
 		nonce += 1
 end_time = perf_counter()
+verify.append([sha256(txs + str(nonce).encode()).hexdigest(), nonce])
 print(sha256(txs + str(nonce).encode()).hexdigest())
 print("nonce: " + str(nonce))
 print ("Execution-time in seconds: " + str(end_time - start_time))
+computation_time += (end_time - start_time)
 print("--------------------------------------------------------------")
 
 print("Three leading zeroes:")
@@ -43,9 +49,11 @@ while(newTry):
 	else:
 		nonce += 1
 end_time = perf_counter()
+verify.append([sha256(txs + str(nonce).encode()).hexdigest(), nonce])
 print(sha256(txs + str(nonce).encode()).hexdigest())
 print("nonce: " + str(nonce))
 print ("Execution-time in seconds: " + str(end_time - start_time))
+computation_time += (end_time - start_time)
 print("--------------------------------------------------------------")
 
 print("Four leading zeroes:")
@@ -58,9 +66,11 @@ while(newTry):
 	else:
 		nonce += 1
 end_time = perf_counter()
+verify.append([sha256(txs + str(nonce).encode()).hexdigest(), nonce])
 print(sha256(txs + str(nonce).encode()).hexdigest())
 print("nonce: " + str(nonce))
 print ("Execution-time in seconds: " + str(end_time - start_time))
+computation_time += (end_time - start_time)
 print("--------------------------------------------------------------")
 
 print("Five leading zeroes:")
@@ -73,9 +83,11 @@ while(newTry):
 	else:
 		nonce += 1
 end_time = perf_counter()
+verify.append([sha256(txs + str(nonce).encode()).hexdigest(), nonce])
 print(sha256(txs + str(nonce).encode()).hexdigest())
 print("nonce: " + str(nonce))
 print ("Execution-time in seconds: " + str(end_time - start_time))
+computation_time += (end_time - start_time)
 print("--------------------------------------------------------------")
 
 print("Six leading zeroes:")
@@ -88,9 +100,11 @@ while(newTry):
 	else:
 		nonce += 1
 end_time = perf_counter()
+verify.append([sha256(txs + str(nonce).encode()).hexdigest(), nonce])
 print(sha256(txs + str(nonce).encode()).hexdigest())
 print("nonce: " + str(nonce))
 print ("Execution-time in seconds: " + str(end_time - start_time))
+computation_time += (end_time - start_time)
 print("--------------------------------------------------------------")
 
 print("Seven leading zeroes:")
@@ -103,7 +117,19 @@ while(newTry):
 	else:
 		nonce += 1
 end_time = perf_counter()
+verify.append([sha256(txs + str(nonce).encode()).hexdigest(), nonce])
 print(sha256(txs + str(nonce).encode()).hexdigest())
 print("nonce: " + str(nonce))
 print ("Execution-time in seconds: " + str(end_time - start_time))
+computation_time += (end_time - start_time)
 print("--------------------------------------------------------------")
+
+print("Verification:")
+start_time = perf_counter()
+for i in verify:
+	print("Safed hash and nonce: ", i)
+	print("New computed hash with safed nonce: " + sha256(txs + str(i[1]).encode()).hexdigest())
+end_time = perf_counter()
+print("--------------------------------------------------------------")
+print("Computation process in seconds: " + str(computation_time))
+print("Verification process in seconds: " + str(end_time - start_time))
