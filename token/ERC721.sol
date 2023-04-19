e81273e3363586739de09661db79cfe9ceb889ea
iemwill <hello@laubenheimer.eu>
2023-04-19 11:27:52 +0200

    rempriv

--- token/ERC721.sol
+++ token/ERC721.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.2.0/contracts/token/ERC721/ERC721.sol";
 
 contract exampleNFT is ERC721 {
-    address private owner;
+    address owner;
     constructor() ERC721 ("Ex-NFT", "ENFT"){
         owner = msg.sender;
         _safeMint(owner, 0, "");
